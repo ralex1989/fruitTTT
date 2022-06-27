89eb0a3256c05c3f3d51facb0d6da0fe6f58381d
ralex1989 <108274520+ralex1989@users.noreply.github.com>
2022-06-27 16:13:04 +0300

    Update TicTacToeInit.js

--- TicTacToeInit.js
+++ TicTacToeInit.js
@@ -179,4 +179,4 @@ class TicTacToe {
 
 const sudoku = new TicTacToe();
 
-document.querySelector( '#wrapper' ).append( sudoku.getHTML( 600 ) );
+document.querySelector( '#wrapper' ).append( sudoku.getHTML( 630 ) );
